b2c986afbc6c7c40cc09807cb0ea05e3ac4c61b5
alexsilva <geniofuturo@gmail.com>
2019-01-07 12:15:04 -0200

    Add setup; package; ignore idea.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Locusfile',
+    version='1.0',
+    packages=['locusfile'],
+    url='https://github.com/alexsilva/locustfile',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='Load tests.'
+)
